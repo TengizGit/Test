a43e8c466137d1080385451316f89b5aa9c63cbc
TengizGit <76786450+TengizGit@users.noreply.github.com>
2021-03-27 15:27:29 +0200

    Task5 border

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,15 +2,12 @@ import React from 'react';
 import '../styles/Article.css';
 
 const firstTitle = 'React internship';
-const secondTitle = '5 урок';
-const thirdParagraph = 'Задание: переписать функции сортировки, добавления и удаления записей биографии из прошлого задания таким образом чтобы все необходимое приходило в качестве параметров (без использования внешних переменных), а функция возвращала новый объект (а не перезаписывала старый). После вызова функции вывести в консоль старый и новый обьекты.';
+
 
 function Article() {
   return (
     <div className="article">
-      <h1>{firstTitle}</h1>
-      <h2>{secondTitle}</h2>
-      <p>{thirdParagraph}</p>
+      <h1>{firstTitle}</h1>      
     </div>
   )
 }
